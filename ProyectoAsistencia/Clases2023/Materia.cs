713f7929bd378cda20371401bd0db8281a326fa0
Facu Cristaldo <cristaldofacundo2016@gmail.com>
2023-05-15 21:22:14 -0300

    Botón agregar

---
+++ ProyectoAsistencia/Clases2023/Materia.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoAsistencia.Clases2023
+{
+    internal class Materia
+    {
+        public int IdMateria { get; set; }
+        public string NombreMateria { get; set; }
+        public int IdProfesor { get; set; }
+        public int IdCurso { get; set; }
+        public int HsCatedra { get; set; }
+    }
+}
---
+++ ProyectoAsistencia/Materias.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProyectoAsistencia
+{
+    /// <summary>
+    /// Lógica de interacción para Materias.xaml
+    /// </summary>
+    public partial class Materias : Window
+    {
+        List<Clases2023.Materia> ListaMaterias = new List<Clases2023.Materia>();
+        public Materias()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAgregar_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Int32 IDVariable = Convert.ToInt32(txtBoxID.Text);
+                Clases2023.Materia ObjetoMateria = ListaMaterias.Where(n => n.IdMateria == IDVariable).FirstOrDefault();
+                if (ObjetoMateria == null)
+                {
+                    ObjetoMateria = new Clases2023.Materia();
+                    ObjetoMateria.IdMateria = Convert.ToInt32(txtBoxID.Text);
+                    ObjetoMateria.NombreMateria = txtBoxMateria.Text;
+                    ObjetoMateria.IdProfesor = cboBoxProfesor.SelectedIndex;
+                    ObjetoMateria.IdCurso = cboBoxCurso.SelectedIndex;
+                    ObjetoMateria.HsCatedra = Convert.ToInt32(txtBoxHs.Text);
+
+                    ListaMaterias.Add(ObjetoMateria);
+                }
+                else
+                {
+                    ObjetoMateria.IdMateria = Convert.ToInt32(txtBoxID.Text);
+                    ObjetoMateria.NombreMateria = txtBoxMateria.Text;
+                    ObjetoMateria.IdProfesor = cboBoxProfesor.SelectedIndex;
+                    ObjetoMateria.IdCurso = cboBoxCurso.SelectedIndex;
+                    ObjetoMateria.HsCatedra = Convert.ToInt32(txtBoxHs.Text);
+                }
+                dtgMaterias.ItemsSource = ListaMaterias;
+                dtgMaterias.Items.Refresh();
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show("Error: " + err.Message, "Aplicación", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
