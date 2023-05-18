a6b2ac5915994f1e4e0fe49846e49c203695d8c0
EspinolaSergio <sergiooespinola016@gmail.com>
2023-05-18 15:48:52 -0300

    Ventana Asistencia y clase

---
+++ ProyectoAsistencia/Clases2023/Asistencia.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoAsistencia.Clases2023
+{
+    internal class Asistencia
+    {
+        public bool Presente { get; set; }
+        public DateTime Fecha { get; set; }
+        public int IdMateria { get; set; }
+        public int IdPreceptor { get; set; }
+        public int IdCurso { get; set; }
+    }
+}
