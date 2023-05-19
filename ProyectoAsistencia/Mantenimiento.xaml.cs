237c5bb58c0e6d550b7b0761e63cdd67ecd44064
agustin torres <agustintorreslollipop@gmail.com>
2023-05-19 18:37:06 -0300

    Menu

--- ProyectoAsistencia/Mantenimiento.xaml.cs
+++ ProyectoAsistencia/Mantenimiento.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Permissions;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -10,24 +11,28 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace ProyectoAsistencia
 {
     /// <summary>
-    /// Lógica de interacción para MainWindow.xaml
+    /// Lógica de interacción para Mantenimiento.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Mantenimiento : Window
     {
-        public MainWindow()
+        public Mantenimiento()
         {
             InitializeComponent();
         }
 
         private void mnuMant_Click(object sender, RoutedEventArgs e)
         {
+            Mantenimiento ventana = new Mantenimiento();
+                ventana.Show();
 
         }
+
+
+
     }
 }
