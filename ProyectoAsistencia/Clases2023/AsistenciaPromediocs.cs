0947f6b80201a9335ec34e840a4978b91a7d6376
davalosvanesa11@gmail.com <davalosvanesa11@gmail.com>
2023-05-19 18:39:01 -0300

    PromedioAsistencias

---
+++ ProyectoAsistencia/Clases2023/AsistenciaPromediocs.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoAsistencia.Clases2023
+{
+    internal class AsistenciaPromediocs
+    {
+    }
+}
