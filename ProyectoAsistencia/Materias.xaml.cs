58fea6d23b75132fd3a77249723802d2bb8ec8fb
Facu Cristaldo <cristaldofacundo2016@gmail.com>
2023-05-19 16:23:23 -0300

    Materias

--- ProyectoAsistencia/Materias.xaml.cs
+++ ProyectoAsistencia/Materias.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using ProyectoAsistencia.Clases2023;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -52,6 +54,67 @@ namespace ProyectoAsistencia
                 }
                 dtgMaterias.ItemsSource = ListaMaterias;
                 dtgMaterias.Items.Refresh();
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show("Error: " + err.Message, "Aplicación", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void btnGuardar_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (File.Exists("Materias.txt"))
+                {
+                    File.Delete("Materias.txt");
+                }
+                string MateriaConcatenado = "";
+                foreach (Materia ObjetoMateria in ListaMaterias)
+                {
+                    MateriaConcatenado = MateriaConcatenado + "\n" + ObjetoMateria.IdMateria + ";" + ObjetoMateria.NombreMateria + ";" +
+                        ObjetoMateria.IdProfesor + ";" + ObjetoMateria.IdCurso + ";" + ObjetoMateria.HsCatedra;
+                }
+                File.WriteAllText("Materias.txt", MateriaConcatenado);
+                MessageBox.Show("Almacenado exitosamente!", "Aplicación", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show("Error al guardar: " + err.Message, "Aplicación", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void btnEliminar_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Materia ObjetoMateria = (Materia)dtgMaterias.SelectedItem;
+                if (ObjetoMateria != null)
+                {
+                    ListaMaterias.Remove(ObjetoMateria);
+                    dtgMaterias.ItemsSource = ListaMaterias;
+                    dtgMaterias.Items.Refresh();
+                }
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show("Error: " + err.Message, "Aplicación", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void dtgMaterias_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            try
+            {
+                Materia ObjetoMateria = (Materia)dtgMaterias.SelectedItem;
+                if (ObjetoMateria != null)
+                {
+                    txtBoxID.Text = ObjetoMateria.IdMateria.ToString();
+                    txtBoxMateria.Text = ObjetoMateria.NombreMateria;
+                    cboBoxProfesor.SelectedIndex = ObjetoMateria.IdProfesor;
+                    cboBoxCurso.SelectedIndex = ObjetoMateria.IdCurso;
+                    txtBoxHs.Text = ObjetoMateria.HsCatedra.ToString();
+                }
             }
             catch (Exception err)
             {
