acb9851124f8983c782f873e78d9d6d930531b69
Facu Cristaldo <cristaldofacundo2016@gmail.com>
2023-05-22 20:28:01 -0300

    .

--- ProyectoAsistencia/Materias.xaml.cs
+++ ProyectoAsistencia/Materias.xaml.cs
@@ -27,6 +27,8 @@ namespace ProyectoAsistencia
             InitializeComponent();
             ClasesPublicas.LeerArchivoMateria();
             dtgMaterias.ItemsSource = ClasesPublicas.ListaMaterias;
+            ClasesPublicas.LeerArchivoCursos();
+            cboBoxCurso.ItemsSource = ClasesPublicas.ListaCursos;
         }
 
         private void btnAgregar_Click(object sender, RoutedEventArgs e)
