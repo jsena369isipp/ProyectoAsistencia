26727df2c1749e2bd364e4a5e45664dc426319a0
davalosvanesa11@gmail.com <davalosvanesa11@gmail.com>
2023-05-29 20:55:00 -0300

    -

---
+++ ProyectoAsistencia/Properties/AsistenciasPromedio.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProyectoAsistencia.Properties
+{
+    /// <summary>
+    /// Lógica de interacción para AsistenciasPromedio.xaml
+    /// </summary>
+    public partial class AsistenciasPromedio : Window
+    {
+        public AsistenciasPromedio()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
