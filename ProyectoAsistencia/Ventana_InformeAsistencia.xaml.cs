9e8f5ffadd5bc72d59a1220de308fbcb68f6d9dd
eliana847 <elianavargas847@gmail.com>
2023-06-28 09:51:31 -0300

    .

--- ProyectoAsistencia/Ventana_InformeAsistencia.xaml.cs
+++ ProyectoAsistencia/Ventana_InformeAsistencia.xaml.cs
@@ -1,4 +1,5 @@
-﻿using ProyectoAsistencia.Clases2023;
+﻿using Microsoft.Reporting.WinForms;
+using ProyectoAsistencia.Clases2023;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -78,6 +79,30 @@ namespace ProyectoAsistencia
             catch (Exception ex)
             {
                 MessageBox.Show("Error!: " + ex.Message, "Aplicacion", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void btnImprimir_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var stream = GetType().Assembly.GetManifestResourceStream("ProyectoAsistencia.Reportes.ListaInfoAsistencia.rdlc");
+                if (stream != null)
+                {
+                    ReportViewer reporViewer = new ReportViewer();
+                    reporViewer.LocalReport.DataSources.Add(new ReportDataSource("DSinformeasistencia", ListaInformeAsistencia));
+                    reporViewer.LocalReport.LoadReportDefinition(stream);
+
+                    reporViewer.Visible = true;
+                    reporViewer.RefreshReport();
+
+                    VentanaReportes ventanaReportes = new VentanaReportes(reporViewer);
+                    ventanaReportes.ShowDialog();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message, "Aplicacion", MessageBoxButton.OK, MessageBoxImage.Error);
             }
         }
 
