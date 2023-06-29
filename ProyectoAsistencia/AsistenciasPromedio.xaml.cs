0456a4f091a6500c6b83b99291b3aa60af0b9a7b
davalosvanesa11@gmail.com <davalosvanesa11@gmail.com>
2023-06-29 19:54:13 -0300

    prpmedioAsistencia

--- ProyectoAsistencia/AsistenciasPromedio.xaml.cs
+++ ProyectoAsistencia/AsistenciasPromedio.xaml.cs
@@ -1,4 +1,5 @@
-﻿using ProyectoAsistencia.Clases2023;
+﻿using Microsoft.Reporting.WinForms;
+using ProyectoAsistencia.Clases2023;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -106,5 +107,30 @@ namespace ProyectoAsistencia
             }
         }
 
+        private void BtnImprimir_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var stream = GetType().Assembly.GetManifestResourceStream("ProyectoAsistencia.Reportes.ListaBuscarAsistencia.rdlc");
+                if (stream != null)
+                {
+
+                    ReportViewer reporViewer = new ReportViewer();
+                    reporViewer.LocalReport.DataSources.Add(new ReportDataSource("DS", listaBuscarAsistencia));
+                    reporViewer.LocalReport.LoadReportDefinition(stream);
+
+                    reporViewer.Visible = true;
+                    reporViewer.RefreshReport();
+
+                    VentanaReportes ventanaReportes = new VentanaReportes(reporViewer);
+                    ventanaReportes.ShowDialog();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message, "Aplicacion", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
     }
 }
+    
