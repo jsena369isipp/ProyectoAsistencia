93d71bbe0b7ebdf008228da30b1ba857d2defeca
Facu Cristaldo <cristaldofacundo2016@gmail.com>
2023-06-29 19:29:48 -0300

    .

--- ProyectoAsistencia/Clases2023/DNIEntidad.cs
+++ ProyectoAsistencia/Clases2023/DNIEntidad.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace ProyectoAsistencia.Clases2023
@@ -13,9 +15,20 @@ namespace ProyectoAsistencia.Clases2023
         public string ApellidoYNombre { get; set; }
         public DateTime FechaDeNacimiento { get; set; }
         public long DNI { get; set; }
+        /*public bool VerificarFormato(string texto)
+        {
+            string patron = @"^\d{11}@[\w\s]+@[\w\s]+@[MF]@\d{8}@.\d{1,2}/\d{1,2}/\d{4}@\d+$";
 
+            return Regex.IsMatch(texto, patron);
+        }*/
         public void DatosDNI(string dniTexto)
         {
+            /*if (!VerificarFormato(dniTexto))
+            {
+                MessageBox.Show("El formato del texto no es válido", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }*/
+
             DNIEntidad ObjetoDNI;
             string txtCompleto = dniTexto;
             ObjetoDNI = new DNIEntidad();
