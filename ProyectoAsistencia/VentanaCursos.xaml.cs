7d1924b774fbe1747dbd60cab53252e21b1a02b3
Facu Espinola <facuespinola210101@gmail.com>
2023-07-03 21:15:08 -0300

    .

--- ProyectoAsistencia/VentanaCursos.xaml.cs
+++ ProyectoAsistencia/VentanaCursos.xaml.cs
@@ -1,4 +1,5 @@
-﻿using ProyectoAsistencia.Clases2023;
+﻿using Microsoft.Reporting.WinForms;
+using ProyectoAsistencia.Clases2023;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -319,7 +320,27 @@ namespace ProyectoAsistencia
 
         private void bttnImprimir_Click(object sender, RoutedEventArgs e)
         {
+            try
+            {
+                var stream = GetType().Assembly.GetManifestResourceStream("ProyectoAsistencia.Reportes.ListaCursos.rdlc");
+                if (stream != null)
+                {
 
+                    ReportViewer reporViewer = new ReportViewer();
+                    reporViewer.LocalReport.DataSources.Add(new ReportDataSource("DSCursos", ListaCursosBuscar));
+                    reporViewer.LocalReport.LoadReportDefinition(stream);
+
+                    reporViewer.Visible = true;
+                    reporViewer.RefreshReport();
+
+                    VentanaReportes ventanaReportes = new VentanaReportes(reporViewer);
+                    ventanaReportes.ShowDialog();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message, "Aplicacion", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
     }
 }
